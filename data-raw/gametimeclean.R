e8e14ad8f2fc87f379b8540542b1ce01e87bee39
EKobrowski <ek2292a@american.edu>
2024-12-08 17:20:04 -0500

    Add files via upload

---
+++ data-raw/gametimeclean.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+# Run Once
+gametimedata <- read_csv("data-raw/gametimedata.csv")
+View(gametimedata)
+names(gametimedata) <- gametimedata[1,]
+gametimedata <- gametimedata[-1,]
+colnames(gametimedata)[10] <- "Location"
+
+# Cleaning
+gametimedata$Location[is.na(gametimedata$Location)] <- "home"
+gametimedata$Location[gametimedata$Location == "@"] <- "away"
+
+write.csv(gametimedata, "updated_gametime.csv")
+
+
+
