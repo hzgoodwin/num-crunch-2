9f3dfdb814c40758a9de5d1a3074f7d82a6081e2
Henry Goodwin <hg5182a@american.edu>
2024-12-08 17:03:56 -0500

    updated

--- data-raw/data_cleaning.R
+++ data-raw/data_cleaning.R
@@ -9,7 +9,20 @@ team_records <- read_csv("./data-raw/team_records.csv")
 names(game_time) <- game_time[1,]
 game_time <- game_time[-1,]
 
+# removing duplicates
+game_time <- distinct(game_time)
+bye_week <- distinct(bye_week)
+
+# fixing types
+game_time$`G#` <- as.numeric(game_time$`G#`)
+game_time$Week <- as.numeric(game_time$Week)
+
 # combining
 temp <- game_time |>
-  select(Team, Date, `Game time`) |>
-  left_join(bye_week, ., by = c("Team", "Date"))
+  select(Team, Date, `G#`, Week, `Game time`) |>
+  inner_join(bye_week, ., by = c("Team", "Date", "G#", "Week"))
+
+# converting to date
+temp$Date <- mdy(temp$Date)
+
+# seasons
