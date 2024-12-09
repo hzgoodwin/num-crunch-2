ca257efb82e3d0564eee1159e61948c5f6c3b3b5
Ethan Kobrowski <ek2292a@american.edu>
2024-12-09 17:13:06 -0500

    fix?

--- data-raw/nfcdivisions.R
+++ data-raw/nfcdivisions.R
@@ -1,4 +1,4 @@
 nfc_east <- ("DAL", "WAS", "NYG", "PHI")
 nfc_west <- ("DET", "MIN", "GNB", "CHI")
 nfc_south <- ("ATL", "TAM", "NOR", "CAR")
-nfc_west <-("SEA", "ARI", "LAR", "SFO")
+nfc_west <- ("SEA", "ARI", "LAR", "SFO")
