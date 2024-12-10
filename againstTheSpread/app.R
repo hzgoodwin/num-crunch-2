2786fcc117ef66162d197c837e47fbcd70721944
Henry Goodwin <hg5182a@american.edu>
2024-12-10 03:13:38 -0500

    interactive plots

--- againstTheSpread/app.R
+++ againstTheSpread/app.R
@@ -4,6 +4,7 @@ library(bslib)
 library(DT)
 library(thematic)
 library(showtext)
+library(plotly)
 
 # Business Logic
 
@@ -39,10 +40,10 @@ ui <- fluidPage(
                sliderInput("away_win_percentage", "Away team win percentage", value = c(0, 1), min = 0, max = 1),
                
                checkboxGroupInput("season", "Season",
-                                  choices = c("2024 (thru 12/5)", "2023", "2022", "2021"),
-                                  selected = c("2024 (thru 12/5)", "2023", "2022", "2021")),
+                                  choices = c("2024", "2023", "2022", "2021"),
+                                  selected = c("2024", "2023", "2022", "2021")),
                
-               sliderInput("week", "Season week", value = c(1, 18), min = 1, max = 18),
+               sliderInput("week", "Week of the season", value = c(1, 18), min = 1, max = 18),
                checkboxGroupInput("day", "Day of the week",
                                   choices = c("Mon", "Tue", "Thu", "Fri", "Sat", "Sun"),
                                   selected = c("Mon", "Tue", "Thu", "Fri", "Sat", "Sun")),
@@ -54,14 +55,14 @@ ui <- fluidPage(
                checkboxInput("after_bye_home", "Games with home team coming off bye only"),
                checkboxInput("after_bye_away", "Games with away team coming off bye only"),
                
-               sliderInput("under_over", "Under/Over", value = c(30, 60), min = 30, max = 60),
+               sliderInput("over_under", "Over/Under", value = c(30, 60), min = 30, max = 60),
                sliderInput("temperature", "Temperature (F)", value = c(0, 110), min = 0, max = 110)
        
                ),
                mainPanel(
                  verbatimTextOutput("results_text"),
-                 plotOutput("spread_vs_actual"),
-                 plotOutput("time_graph")
+                 plotlyOutput("spread_vs_actual"),
+                 plotlyOutput("time_graph")
                )
              )
     ),
@@ -86,48 +87,76 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     filtered_data <- reactive({
-      filtered <- data
-      
-      if (input$home_team != "--All--"){
-        filtered <- filtered |> filter(home_team == !!input$hometeam)
-      }
-      
-      filtered
-      
+      data |>
+        filter(
+          input$home_team == "--All--" | home_team == input$home_team,
+          input$away_team == "--All--" | away_team == input$away_team,
+          spread_home >= input$home_spread[1] & 
+            spread_home <= input$home_spread[2],
+          win_percentage_home >= input$home_win_percentage[1] & 
+            win_percentage_home <= input$home_win_percentage[2],
+          win_percentage_away >= input$away_win_percentage[1] & 
+            win_percentage_away <= input$away_win_percentage[2],
+          season %in% input$season,
+          week >= input$week[1] & week <= input$week[2],
+          day %in% input$day,
+          game_time %in% input$game_time,
+          input$divisional == FALSE | divisional == TRUE,
+          input$after_bye_home == FALSE | after_bye_home == TRUE,
+          input$after_bye_away == FALSE | after_bye_away == TRUE,
+          over_under >= input$over_under[1] & over_under <= input$over_under[2],
+          temperature >= input$temperature[1] & temperature <= input$temperature[2]
+        )
     })
+  
 
     model_formula <- reactive({
       req(input$X_var)
       as.formula(paste0("spread" ~ paste(input$X_var, collapse = " + ")))
     })
     
-    output$spread_vs_actual <- renderPlot({
-      ggplot(data, aes(spread_home, actual_result_home)) +
+    output$spread_vs_actual <- renderPlotly({
+      p1 <- ggplot(filtered_data(), aes(spread_home, actual_result_home, 
+                                  text = paste0(
+                                    season, " week ", week, "<br>",
+                                    home_team, " ", str_extract(result_home, "\\d+-\\d+"),
+                                    " ", away_team, "<br>",
+                                    "Home team spread: ", spread_home, "<br>",
+                                    "Home team result: ", actual_result_home))) +
         geom_point(alpha = .4) +
-        geom_abline(slope=1, color= "blue")
+        geom_abline(slope=1, color= "blue") +
+        geom_label(aes(label = "Home team covered"), vjust = -1, hjust = 0.5)
+      ggplotly(p1, tooltip = "text")
+        
       })
     
     output$results_text <- renderText({
       paste0(
       "Total games selected: ", nrow(filtered_data()), "\n",
       "Percent of games in which the home team covered the spread: ",
-      round(filter(data, vs_line_home == "covered") |> nrow()*100 /nrow(data), 2), "%\n",
+      round(filter(filtered_data(), vs_line_home == "covered") |> nrow()*100 /nrow(filtered_data()), 2), "%\n",
       "Percent of games in which the away team covered the spread: ",
-      round(filter(data, vs_line_away == "covered") |> nrow()*100 /nrow(data), 2), "%\n",
+      round(filter(filtered_data(), vs_line_away == "covered") |> nrow()*100 /nrow(filtered_data()), 2), "%\n",
       "Percent of games where the teams pushed: ",
-      round(filter(data, vs_line_home == "push") |> nrow()*100 /nrow(data), 2), "%"
+      round(filter(filtered_data(), vs_line_home == "push") |> nrow()*100 /nrow(filtered_data()), 2), "%"
       )
     })
     
-    output$time_graph <- renderPlot({
-      data |>
-        mutate(month_day = format(date, "%m-%d")) |>
-        ggplot(aes(x = month_day, y = actual_result_home, color = as.factor(season))) +
+    output$time_graph <- renderPlotly({
+      p2 <- filtered_data() |>
+        ggplot(aes(x = date, y = actual_result_home, text = paste0(
+          season, " week ", week, "<br>",
+          home_team, " ", str_extract(result_home, "\\d+-\\d+"),
+          " ", away_team, "<br>",
+          "Home team spread: ", spread_home, "<br>",
+          "Home team result: ", actual_result_home))) +
         geom_point(alpha = 0.6) +
-        scale_x_discrete(
-          labels = scales::date_format("%m-%d")
+        scale_x_date(
+          breaks = "1 month",
+          limits = range(filtered_data()$date)
         ) +
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      ggplotly(p2, tooltip = "text")
     })
 
 }
