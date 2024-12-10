23e0369f637a2f52eeb0d940a37d3f62845164ad
Henry Goodwin <hg5182a@american.edu>
2024-12-10 01:47:30 -0500

    basic inputs

--- againstTheSpread/app.R
+++ againstTheSpread/app.R
@@ -9,6 +9,8 @@ library(showtext)
 
 ## Read Data, Clean Data
 data <- read_csv("../data/data.csv")
+
+teams <- c("--All--", str_sort(unique(data$home_team)))
 # cleaning done in final_cleaning.R file
 
 ## UI
@@ -25,10 +27,36 @@ ui <- fluidPage(
     tabPanel("Actual results v. Vegas Spread",
              sidebarLayout(
                sidebarPanel(
-                 textOutput("This tab allows the user to filter by various characteristics to see how vegas spread compares to actual spread based on selected characteristics.")
                  
+               # variable inputs
                  
-                 # Add inputs for variables we choose
+               selectInput("home_team", "Select home team", choices = teams),
+               selectInput("away_team", "Select away team", choices = teams),
+               
+               sliderInput("home_spread", "Home team spread", value = c(-20, 20), min = -20, max = 20),
+               
+               sliderInput("home_win_percentage", "Home team win percentage", value = c(0, 1), min = 0, max = 1),
+               sliderInput("away_win_percentage", "Away team win percentage", value = c(0, 1), min = 0, max = 1),
+               
+               checkboxGroupInput("season", "Season",
+                                  choices = c("2024 (thru 12/5)", "2023", "2022", "2021"),
+                                  selected = c("2024 (thru 12/5)", "2023", "2022", "2021")),
+               
+               sliderInput("week", "Season week", value = c(1, 18), min = 1, max = 18),
+               checkboxGroupInput("day", "Day of the week",
+                                  choices = c("Mon", "Tue", "Thu", "Fri", "Sat", "Sun"),
+                                  selected = c("Mon", "Tue", "Thu", "Fri", "Sat", "Sun")),
+               checkboxGroupInput("game_time", "Game time",
+                                  choices = c("early", "afternoon", "night"),
+                                  selected = c("early", "afternoon", "night")),
+               
+               checkboxInput("divisional", "Divisional games only"),
+               checkboxInput("after_bye_home", "Games with home team coming off bye only"),
+               checkboxInput("after_bye_away", "Games with away team coming off bye only"),
+               
+               sliderInput("under_over", "Under/Over", value = c(30, 60), min = 30, max = 60),
+               sliderInput("temperature", "Temperature (F)", value = c(0, 110), min = 0, max = 110)
+       
                ),
                mainPanel(
                  verbatimTextOutput("results_text"),
@@ -57,6 +85,17 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
 
+    filtered_data <- reactive({
+      filtered <- data
+      
+      if (input$home_team != "--All--"){
+        filtered <- filtered |> filter(home_team == !!input$hometeam)
+      }
+      
+      filtered
+      
+    })
+
     model_formula <- reactive({
       req(input$X_var)
       as.formula(paste0("spread" ~ paste(input$X_var, collapse = " + ")))
@@ -70,11 +109,12 @@ server <- function(input, output) {
     
     output$results_text <- renderText({
       paste0(
+      "Total games selected: ", nrow(filtered_data()), "\n",
       "Percent of games in which the home team covered the spread: ",
       round(filter(data, vs_line_home == "covered") |> nrow()*100 /nrow(data), 2), "%\n",
       "Percent of games in which the away team covered the spread: ",
       round(filter(data, vs_line_away == "covered") |> nrow()*100 /nrow(data), 2), "%\n",
-      "Percent of games in where the teams pushed: ",
+      "Percent of games where the teams pushed: ",
       round(filter(data, vs_line_home == "push") |> nrow()*100 /nrow(data), 2), "%"
       )
     })
