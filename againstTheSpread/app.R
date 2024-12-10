35acc86a9734733e2195d1d33358b5b54342e28c
Henry Goodwin <hg5182a@american.edu>
2024-12-09 20:08:46 -0500

    referenced data file

--- againstTheSpread/app.R
+++ againstTheSpread/app.R
@@ -8,10 +8,10 @@ library(showtext)
 # Business Logic
 
 ## Read Data, Clean Data
+data <- read_csv("../data/data.csv")
+# cleaning done in final_cleaning.R file
 
-
-
-# Define UI for application that draws a histogram
+## UI
 ui <- fluidPage(
 
   theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
@@ -21,10 +21,13 @@ ui <- fluidPage(
       "Introduction",
       textOutput("Add text that explains sports betting terms and the app's usage; see peer review of our group")
     ),
-    tabPanel("Actual Spread v. Vegas Spread",
+    
+    tabPanel("Actual results v. Vegas Spread",
              sidebarLayout(
                sidebarPanel(
                  textOutput("This tab allows the user to filter by various characteristics to see how vegas spread compares to actual spread based on selected characteristics.")
+                 
+                 
                  # Add inputs for variables we choose
                ),
                mainPanel(
@@ -36,20 +39,8 @@ ui <- fluidPage(
     tabPanel("Spread v. User Selected Variables",
              sidebarLayout(
                sidebarPanel(
-                 varSelectInput("ActualX_var", "For the Multiple Regression Plot, select the variable for the X axis", data = (NA)),
-                 varSelectInput("ActualY_var", "For the Multiple Regression Plot, select the variable for the Y axis", data = (NA))
-               ),
-               mainPanel(
-                 plotOutput("MLR_plot"),
-                 verbatimTextOutput("lm_sum")
-               )
-             )
-    ),
-    tabPanel("Spread v. User Selected Variables",
-             sidebarLayout(
-               sidebarPanel(
                  checkboxGroupInput("X_var", "Report Year of Interest:", 
-                                    choices = names(dataset),
+                                    choices = names(data),
                                     selected = "NA"),
                  varSelectInput("ActualY_var", "For the Multiple Regression Plot, select the variable for the Y axis", data = (NA))
                ),
@@ -69,6 +60,9 @@ server <- function(input, output) {
       req(input$X_var)
       as.formula(paste0("spread" ~ paste(input$X_var, collapse = " + ")))
     })
+    
+    output$two_var_plot <- renderPlot({
+    
 }
 
 # Run the application 
